8039ef0bb04fbdab270df63a6ccd2d406c0061a7
belousov <belousovsergej56@gmail.com>
2025-06-03 18:30:38 +0300

    добавил настройку расширения файла, добавил настройку редактора

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,13 @@
 import tomllib
-import os
+
+path_to_config = "config.toml"
 
 class Config:
-    with open("config.toml", "rb") as f:
+    with open(path_to_config, "rb") as f:
         config_data = tomllib.load(f)
-
+    
     PATH_TO_STORAGE = config_data["backend"]["path_to_storage_directory"]
     PATH_TO_CONFIG_TN = config_data["backend"]["path_to_config_tn"]
+    EXTENSION = config_data["backend"]["file_extension"]
     PATH_TO_TEMPLATE_FILE = config_data["template"]["path_to_template_note"]
-
+    EDITOR = config_data["editor"]["editor"]
